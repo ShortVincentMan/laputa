d6b61c0b0c6f44711b8b31e2119bad9213286de2
ShortVincentMan <vinvyle06@gmail.com>
2026-07-25 12:48:43 -0700

    feat: releasing build 1.06

--- src/data/build.ts
+++ src/data/build.ts
@@ -1,6 +1,6 @@
 export const BUILD = {
-  display: "01.05",
-  semver: "1.0.5",
+  display: "01.06",
+  semver: "1.0.6",
   date: "2026-07-25",
-  title: "Quickhacks Expansion",
+  title: "Personality Expansion",
 } as const;
